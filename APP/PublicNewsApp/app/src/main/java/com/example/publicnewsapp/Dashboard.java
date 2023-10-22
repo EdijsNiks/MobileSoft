a1202f65fcfc6357f829d7bebf181a3997436d8c
OtoJauja <97533069+OtoJauja@users.noreply.github.com>
2023-10-23 01:21:09 +0300

    Now it should work

--- APP/PublicNewsApp/app/src/main/java/com/example/publicnewsapp/Dashboard.java
+++ APP/PublicNewsApp/app/src/main/java/com/example/publicnewsapp/Dashboard.java
@@ -9,23 +9,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Dashboard extends AppCompatActivity {
 
-    Button btn_readNews, btn_generalNews, btn_publishNews;
+    Button  btn_generalNews, btn_publishNews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        btn_readNews = findViewById(R.id.btn_readNews);
+        //btn_readNews = findViewById(R.id.btn_readNews);
         btn_publishNews = findViewById(R.id.btn_publishNews);
-        //btn_generalNews = findViewById(R.id.btn_generalNews);
+        btn_generalNews = findViewById(R.id.btn_generalNews);
 
-        btn_readNews.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Dashboard.this, ReadNews.class));
-            }
-        });
+        //btn_readNews.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View v) {
+                //startActivity(new Intent(Dashboard.this, ReadNews.class));
+            //}
+        //});
         btn_publishNews.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
