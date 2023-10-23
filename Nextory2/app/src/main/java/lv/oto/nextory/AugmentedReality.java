441cbc2b89a4830b10bdc923aca83de67f5194a8
EdijsNiks <edijsniks@gmail.com>
2023-10-23 21:55:27 +0300

    Added splash screen and hidden password

---
+++ Nextory2/app/src/main/java/lv/oto/nextory/AugmentedReality.java
@@ -0,0 +1,19 @@
+package lv.oto.nextory;
+
+import com.google.ar.sceneform.ArFragment;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AugmentedReality extends AppCompatActivity {
+
+    private ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+    }
+}
